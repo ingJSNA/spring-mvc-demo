a7675600221d43c2001dc0e1754210c662deeff3
Juan Navarro <jsnavarroa@unal.edu.co>
2022-09-07 12:08:21 -0500

    Add missing edit capabilities

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-FROM tomcat:8.0.20-jre8
-#FROM tomee:8-jre-1.7.2-webprofile
+FROM tomcat:9-jre8
+#FROM tomee:8-jre8-webprofile
 
 COPY /target/SpringMVCDemo.war /usr/local/tomcat/webapps/
 #COPY /target/SpringMVCDemo.war /usr/local/tomee/webapps/
--- src/main/java/com/ingjsna/view/ProductBean.java
+++ src/main/java/com/ingjsna/view/ProductBean.java
@@ -5,6 +5,7 @@ import com.ingjsna.dao.ProductDAO;
 import com.ingjsna.dao.ProductDAOImpl;
 import com.ingjsna.service.ProductService;
 import com.ingjsna.service.ProductServiceImpl;
+import org.primefaces.event.RowEditEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,47 +17,61 @@ import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 import org.springframework.web.jsf.FacesContextUtils;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 
-@Named("productBean")
-@ViewScoped
-public class ProductBean implements Serializable{
+@Named
+@SessionScoped
+public class ProductBean {
 
     private static final Logger logger = LoggerFactory.getLogger(ProductBean.class);
 
     @Inject()
     @Qualifier(value="productService")
-    //@ManagedProperty("#{productService}")
     private ProductService service;
 
-    private List<Product> products = Collections.emptyList();
+    private List<Product> products = new ArrayList<>();
 
     private Product newProduct = new Product();
 
     @PostConstruct
     public void init()  {
         products = service.listProducts();
+
         logger.error("Products: " + products.size());
     }
 
-    public void createProduct(String name, String description){
+    public void createProduct(){
         service.addProduct(newProduct);
+        newProduct = new Product();
+    }
+    public void onRowEdit(RowEditEvent<Product> event) {
+        Product product = event.getObject();
+        service.updateProduct(product);
+
+        FacesMessage msg = new FacesMessage("Producto editado", String.valueOf(product.getId()));
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public void remove(Product product){
+        service.removeProduct(product.getId());
     }
 
     public List<Product> getProducts() {
-        logger.error("Get Products");
-        return service.listProducts();
+        return products;
     }
 
     public ProductService getService() {
